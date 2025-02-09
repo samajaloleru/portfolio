b21de8a3d610991b33f686957dfc3b4e9e9ff2c0
Ajaloleru Samson <37838638+samajaloleru@users.noreply.github.com>
2025-02-09 08:32:32 +0100

    updated about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -7,9 +7,9 @@ import { FacebookLogo, GithubLogo, InstagramLogo, LinkedinLogo } from "@phosphor
 export default function About(): JSX.Element {
  
   return (
-    <div className={`flex flex-col gap-10 items-center w-full z-30 text-white pt-10`} >
+    <div className={`flex flex-col gap-10 items-center w-full z-30 text-white pb-10`} >
       <div className="flex flex-col md:flex-row items-start gap-10 w-full">
-        <div className="bg-secondary rounded-[2rem] sticky gap-5 flex flex-col md:w-1/2 p-7">
+        <div className="bg-secondary rounded-[2rem] static gap-5 flex flex-col md:w-1/2 p-7">
           <img className="aspect-square md:h-[25rem] w-full object-cover rounded-[2rem]" alt="logo" src={logo} />
           <div className="flex flex-col items-center w-full gap-3 text-white">
             <div className="md:text-[2rem] text-2xl leading-tight font-semibold">
@@ -36,14 +36,26 @@ export default function About(): JSX.Element {
           </div>
         </div>
 
-        <div className="flex flex-col lg:w-1/2 gap-10">
+        <div className="flex flex-col md:w-1/2 gap-10">
           <div className="flex flex-col w-full gap-10">
-            <div className="text-lg">ABOUT ME</div>
+            <div className="text-lg font-medium">ABOUT ME</div>
             <div className="opacity-75 tracking-wide leading-6">
               As a Senior Software Engineer with 7+ years of hands-on experience, I specialize in architecting and delivering high-performance, full-stack web applications that drive business growth and user engagement. Proficient in modern frontend frameworks like React.js, Next.js, Vue.js, Nuxt.js, and TypeScript, I build intuitive, responsive interfaces optimized for performance and accessibility. On the backend, I leverage Node.js, Laravel, and REST/GraphQL APIs to design scalable microservices and cloud-native solutions, with expertise in AWS, Docker, and CI/CD pipelines. My experience spans industries such as fintech, e-commerce, and SaaS, where I’ve successfully led cross-functional teams to deploy solutions handling millions of users, while adhering to Agile methodologies, Test-Driven Development (TDD), and DevOps best practices
             </div>
             <div className="opacity-75 tracking-wide leading-6">
               Beyond technical execution, I thrive in collaborative environments, bridging gaps between product, design, and engineering teams to align technical outcomes with strategic goals. My passion for innovation is matched by a commitment to clean code, robust documentation, and mentoring junior developers. I’ve spearheaded initiatives to migrate legacy systems to modern architectures, optimize application performance by 40%+, and integrate AI-driven features like chatbots and predictive analytics. A lifelong learner, I stay ahead of industry trends through certifications in cloud technologies and contributions to open-source projects, ensuring solutions are both cutting-edge and sustainable.
+            </div>
+          </div>
+
+          <div className="flex flex-col w-full gap-10">
+            <div className="text-lg font-medium">EXPERIENCE</div>
+            <div className="flex flex-col w-full gap-2">
+              <div className="">2024</div>
+              <div className="text-light-green">Senior Software Engineer</div>
+              <div className="">Aider</div>
+              <div className="opacity-75 tracking-wide leading-6">
+                Spearheaded the enhancement of the company's core platform by leading the development of order fulfillment, inventory management, and customer support integration features, implementing a JWT-based authentication system to bolster security and data privacy, optimizing code performance by 15% through profiling and caching, and collaborating cross-functionally to ensure on-time delivery aligned with business objectives.
+              </div>
             </div>
           </div>
         </div>
