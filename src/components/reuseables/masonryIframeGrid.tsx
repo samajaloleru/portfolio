f2a4d900a009b6475635974c68d2900110ac8cda
Ajaloleru Samson <37838638+samajaloleru@users.noreply.github.com>
2025-07-04 17:49:57 +0100

    Fix URL scheme for Omotola Cathedral Member Portal in MasonryIframeGrid

--- src/components/reuseables/masonryIframeGrid.tsx
+++ src/components/reuseables/masonryIframeGrid.tsx
@@ -9,7 +9,7 @@ const MasonryIframeGrid = () => {
     { url: 'https://www.ectc.ltd', title: 'ECTC' },
     { url: 'https://goldaesthetics.uk/', title: 'Gold Aesthetics' },
     { url: 'https://amobileadership.org/', title: 'Amobi Eductaion Foundation' },
-    { url: 'http://omotola-ui-app.vercel.app/', title: 'C.C.C. Omotola Cathedral Member Portal' },
+    { url: 'https://omotola-ui-app.vercel.app/', title: 'C.C.C. Omotola Cathedral Member Portal' },
   ];
 
 //   const mobileViewport = `
